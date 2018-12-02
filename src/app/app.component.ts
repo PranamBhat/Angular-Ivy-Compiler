21343f8cac6894bc52cd0944f78af2c84168dbdf
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-12-02 17:48:50 -0400

    🎉 init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Component ,
+  ɵrenderComponent as renderComponent,
+  ɵComponentType as ComponentType
+} from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: 'app-hello',
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+  `,
 })
 export class AppComponent {
   title = 'angular-ivy';
 }
+
+renderComponent(AppComponent as ComponentType<AppComponent>);
